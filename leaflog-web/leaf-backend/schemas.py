a2db0e21b78e1f145a9d292a7785986d5b2d4c55
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-02-06 21:37:28 +0530

    Create schemas.py

---
+++ leaflog-web/leaf-backend/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class EnergyUsageBase(BaseModel):
+    month: int
+    year: int
+    electricity_kwh: float
+    renewable_kwh: float
+
+class EnergyUsageCreate(EnergyUsageBase):
+    household_id: int
+
+class EnergyUsageResponse(EnergyUsageBase):
+    usage_id: int
+    household_id: int
+
+    class Config:
+        orm_mode = True
