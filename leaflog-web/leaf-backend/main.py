9e07464a9d667bd5e8d197bc88f79e6228dab83b
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-02-09 16:16:30 +0530

    Update main.py

--- leaflog-web/leaf-backend/main.py
+++ leaflog-web/leaf-backend/main.py
@@ -1,7 +1,37 @@
 from fastapi import FastAPI
 from database import get_connection
+from models import Household
+import database
+import schemas
+
+from pydantic import BaseModel
+
+
 
 app = FastAPI(title = 'Leaflog Sustainability API')
+
+@app.post("/households")
+def create_household(household: Household):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    query = """
+    INSERT INTO Households (household_name, city_zone, members_count)
+    VALUES (%s, %s, %s)
+    """
+
+    cursor.execute(query, (
+        household.household_name,
+        household.city_zone,
+        household.members_count
+    ))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return {"message": "Household created"}
+
 
 @app.get('/households')
 def get_households():
@@ -68,6 +98,92 @@ def sustainability_summary():
     cursor.close()
     conn.close()
     return data
+
+
+households = []
+
+@app.post("/households")
+def create_household(data: Household):
+    households.append(data)
+    return {"message": "Household created"}
+
+@app.get("/households")      # GET APIs simply fetch data, they dont display a body just the fetched data.
+def get_households():
+    return households
+
+@app.get("/households/{household_id}")
+def get_household(household_id: int):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM Households WHERE household_id=%s", (household_id,))
+    result = cursor.fetchone()
+
+    cursor.close()
+    conn.close()
+
+    return result
+
+
+@app.get("/households")
+def get_all_households():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM Households")
+    households = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return households
+
+@app.put("/households/{household_id}")
+def update_household(household_id: int, household_name: str):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "UPDATE Households SET household_name=%s WHERE household_id=%s",
+        (household_name, household_id)
+    )
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+
+    return {"message": "Household updated successfully"}
+
+class EnergyUsage(BaseModel):
+    household_id: int
+    electricity_kwh: float
+    month: int
+    year: int
+
+@app.post("/energy-usage")
+def add_energy_usage(data: EnergyUsage):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    query = """
+    INSERT INTO EnergyUsage (household_id, month, year, electricity_kwh)
+    VALUES (%s, %s, %s, %s)
+    """
+
+    cursor.execute(query, (
+        data.household_id,
+        data.month,
+        data.year,
+        data.electricity_kwh
+    ))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return {"message": "Energy usage added successfully"}
+
+
 
 @app.get("/")
 def home():
