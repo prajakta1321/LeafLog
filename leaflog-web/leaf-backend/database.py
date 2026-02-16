c66495319883cb30d1bf270b5fc18aeb613444b9
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-02-16 20:17:12 +0530

    Update database.py

--- leaflog-web/leaf-backend/database.py
+++ leaflog-web/leaf-backend/database.py
@@ -1,3 +1,4 @@
+# old version
 import mysql.connector
 
 def get_connection():
@@ -7,3 +8,28 @@ def get_connection():
         password="root",
         database="leaflog"
     )
+
+# new version
+
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="leaflog"
+    )
+
+def create_energy_usage(usage):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO EnergyUsage (household_id, electricity_kwh, month) VALUES (%s, %s, %s)",
+        (usage.household_id, usage.electricity_kwh, usage.gas_units, usage.month))
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return {"message" : "Energy usage is added."}
