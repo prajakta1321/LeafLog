bdf25360b49501b0107f87954d3847a47bf58d38
prajakta1321 <91279433+prajakta1321@users.noreply.github.com>
2026-02-18 11:48:21 +0530

    Update schemas.py

--- leaflog-web/leaf-backend/schemas.py
+++ leaflog-web/leaf-backend/schemas.py
@@ -1,3 +1,4 @@
+# old version
 from pydantic import BaseModel
 from typing import Optional
 
@@ -16,3 +17,30 @@ class EnergyUsageResponse(EnergyUsageBase):
 
     class Config:
         orm_mode = True
+
+
+# new version
+
+from pydantic import BaseModel
+from typing import Optional
+
+class EnergyUsageBase(BaseModel):
+    month: int
+    year: int
+    electricity_kwh: float
+    renewable_kwh: float
+
+class EnergyUsageCreate(EnergyUsageBase):
+    household_id: int
+
+class EnergyUsageResponse(EnergyUsageBase):
+    usage_id: int
+    household_id: int
+
+    class Config:
+        orm_mode = True
+
+class EnergyUsage(BaseModel):
+    household_id : int
+    electricity_kwh : float
+    month : str
